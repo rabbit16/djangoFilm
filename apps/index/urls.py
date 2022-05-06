903949a3e0e63f33ff5aa809856ff28afb006ebd
rabbit16 <1214662198@qq.com>
2022-05-06 10:56:59 +0800

    首页

---
+++ apps/index/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/5/6 10:51 上午
+# @Author  : rabbit
+# @File    : urls.py
+# @Software: PyCharm
+
+from django.urls import path, re_path
+from index import views
+app_name = 'index'
+urlpatterns = [
+    path('', views.index.as_view(), name='see_index')
+]
