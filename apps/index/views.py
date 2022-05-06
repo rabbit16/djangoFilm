a820430143c4d990aa1aa0c1de1cfe85fbd08b4b
rabbit16 <1214662198@qq.com>
2022-05-06 11:01:26 +0800

    首页

---
+++ apps/index/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import View
+
+
+class index(View):
+
+    def get(self, request):
+        return render(request, "index/index.html")
