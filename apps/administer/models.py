884c96e8be61a8a453d05cb1af5387b952bbbd12
rabbit16 <1214662198@qq.com>
2022-05-18 20:45:26 +0800

    建立了表单以及他们的关联关系

--- apps/administer/models.py
+++ apps/administer/models.py
@@ -1,39 +1,48 @@
 from django.db import models
 from django.utils import timezone
+from users.models import User
+from users.models import Movie
 
-class Ticket:
-    Ticket_id = models.CharField(max_length=20,help_text="电影票id",verbose_name="电影票id")
-    Movie_id = models.CharField(max_length=20, help_text="电影id", verbose_name="电影id")
-    Seat_id = models.CharField(max_length=20, help_text="座位id", verbose_name="座位id")
-    Studio_id = models.CharField(max_length=20, help_text="演播厅id", verbose_name="演播厅id")
+class Ticket(models.Model):
+    Ticket_id = models.IntegerField(max_length=20,help_text="电影票id",verbose_name="电影票id")
+    Seat_id = models.IntegerField(max_length=20, help_text="座位id", verbose_name="座位id")
+    Studio_id = models.IntegerField(max_length=20, help_text="演播厅id", verbose_name="演播厅id")
     Movie_name = models.CharField(max_length=20, verbose_name='电影名' , help_text="电影名")
-    Movie_time = models.CharField(max_length=20, verbose_name="电影时间",help_text="电影时间")
-    s_user = models.ForeignKey('User', on_delete=models.CASCADE)
+    Movie_time = models.DateTimeField(verbose_name="电影时间",help_text="电影时间")
+    s_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.FloatField(verbose_name="电影票的价格", help_text="电影票的价格")
     class Meta:
         db_table = 'tb_Ticket'
         verbose_name = '电影票'
-class Session:
-    S_user = models.ManyToManyField('Users')
-    S_studio=models.ManyToManyField('Studio')
+
+    def __str__(self):
+        return self.Ticket_id
+class Times(models.Model):  # 电影场次
+    S_user = models.ManyToManyField(User)
+    S_studio = models.ManyToManyField('Studio')
     S_seat = models.ManyToManyField('Seat')
-    S_movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
-class Seat:
-    Seat_id = models.CharField(max_length=20, help_text="座位id", verbose_name="座位id")
-    mark = models.BooleanField(verbose_name="占用情况", help_text="占用情况", default=False)
-    #fk连接studio
-    #Fk连接用户
-    s_stu = models.ForeignKey('Studio', on_delete=models.CASCADE)
-    s_user = models.ForeignKey('User', on_delete=models.CASCADE, default=None)
+    S_movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    session = models.IntegerField(verbose_name="场次", help_text="场次")
+
+    class Meta:
+        db_table = "tb_session"
+        verbose_name = "电影场次"
+
+class Seat(models.Model):
+    Seat_id = models.IntegerField(max_length=20, help_text="座位id", verbose_name="座位id")
+    Seat_name = models.CharField(max_length=20, verbose_name="座位编号", help_text="座位编号")
+    s_stu = models.ManyToManyField('Studio')
     class Meta:
         db_table = 'tb_Seat'
         verbose_name = '座位'
+    def __str__(self):
+        return self.Seat_id
 
-class Studio:
+class Studio(models.Model):
     Studio_id = models.CharField(max_length=20,help_text="演播厅id",verbose_name="演播厅id")
-    Studio_name= models.CharField(max_length=20, help_text="演播厅名称", verbose_name="演播厅名称")
+    Studio_name = models.CharField(max_length=20, help_text="演播厅名称", verbose_name="演播厅名称")
     Studio_type = models.CharField(max_length=20, help_text="演播厅类型", verbose_name="演播厅类型")
     Seating = models.CharField(max_length=20, help_text="座位个数", verbose_name="座位个数")
-
     class Meta:
         db_table = 'tb_Studio'
         verbose_name = '演播厅'
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,26 +1,44 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import UserManager as _UserManager, AbstractUser
 
-class Users:
-    Users_id = models.CharField(max_length=20,help_text="用户id",verbose_name="用户id")
-    Users_name = models.CharField(max_length=20, verbose_name='用户昵称' , help_text="用户昵称")
-    Name = models.CharField(max_length=20, verbose_name='真实名称', help_text="真实名称")
-    Users_sex = models.BooleanField(verbose_name="性别", help_text="性别", default=True)
-    Users_email = models.CharField(max_length=20, verbose_name='用户邮箱', help_text="用户邮箱")
-    Users_password = models.CharField(max_length=20, verbose_name='用户密码', help_text="用户密码")
-    mobile = models.CharField(max_length=20, verbose_name='电话号码', help_text="电话号码")
-    registration_date = models.CharField(max_length=20, verbose_name='注册日期', help_text="注册日期")
-    ticketmanager = models.BooleanField(verbose_name="票务管理员", help_text="票务管理员", default=False)
-    supermanager = models.BooleanField(verbose_name="超级管理员", help_text="超级管理员", default=False)
-    buymanager = models.BooleanField(verbose_name="售票员", help_text="售票员", default=False)
+# Create your models here.
 
+class UserManager(_UserManager):
+    def create_superuser(self, username, password, email=None, **extra_fields):
+        super().create_superuser(username=username, email=email, password=password, **extra_fields)
+
+class User(AbstractUser):
+    objects = UserManager()
+    REQUIRED_FIELDS = ['mobile']
+    name = models.CharField(max_length=20,
+                            help_text="用户真实姓名",
+                            verbose_name="用户真实姓名",
+                            error_messages={"message": "名字格式出错"}
+                            )
+    ticketManager = models.BooleanField(verbose_name="票务管理员", help_text="票务管理员", default=False)
+    buyManager = models.BooleanField(verbose_name="售票员", help_text="售票员", default=False)
+    mobile = models.CharField(max_length=11, verbose_name='手机号', help_text="手机号", unique=True,
+                              error_messages={'unique': "该手机号已注册"})
+    email_ac = models.BooleanField(default=False, verbose_name="邮箱状态")
+    registration_data = models.DateTimeField(verbose_name="注册时间",
+                                             help_text="注册时间",
+                                             default=timezone.now
+                                             )
+    sex = models.BooleanField(verbose_name="性别", help_text="性别", default=True)
+    birthday = models.DateTimeField(verbose_name="生日", help_text="生日", default=timezone.now)
     class Meta:
-        db_table = 'tb_Users'
+        db_table = 'tb_users'
         verbose_name = '用户'
-class Movie:
-    Movie_id = models.CharField(max_length=20,help_text="电影id",verbose_name="电影id")
-    Movie_name = models.CharField(max_length=20, verbose_name='电影名' , help_text="电影名")
-    Movie_time = models.CharField(max_length=20, verbose_name="电影时间",help_text="电影时间")
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    Movie_id = models.CharField(max_length=20,help_text="电影id", verbose_name="电影id")
+    Movie_name = models.CharField(max_length=20, verbose_name='电影名', help_text="电影名")
+    Movie_time = models.CharField(max_length=20, verbose_name="电影上映时间", help_text="电影上映时间")
 
     class Meta:
         db_table = 'tb_Movie'
