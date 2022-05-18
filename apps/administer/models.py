6e3e99ee2d0cb0f5d24ff806df2cd52a46bb0ef5
rabbit16 <1214662198@qq.com>
2022-05-18 19:12:16 +0800

    我创建了6个表，备份了两个文件

--- apps/administer/models.py
+++ apps/administer/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Ticket:
+    Ticket_id = models.CharField(max_length=20,help_text="电影票id",verbose_name="电影票id")
+    Movie_id = models.CharField(max_length=20, help_text="电影id", verbose_name="电影id")
+    Seat_id = models.CharField(max_length=20, help_text="座位id", verbose_name="座位id")
+    Studio_id = models.CharField(max_length=20, help_text="演播厅id", verbose_name="演播厅id")
+    Movie_name = models.CharField(max_length=20, verbose_name='电影名' , help_text="电影名")
+    Movie_time = models.CharField(max_length=20, verbose_name="电影时间",help_text="电影时间")
+    s_user = models.ForeignKey('User', on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'tb_Ticket'
+        verbose_name = '电影票'
+class Session:
+    S_user = models.ManyToManyField('Users')
+    S_studio=models.ManyToManyField('Studio')
+    S_seat = models.ManyToManyField('Seat')
+    S_movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
+class Seat:
+    Seat_id = models.CharField(max_length=20, help_text="座位id", verbose_name="座位id")
+    mark = models.BooleanField(verbose_name="占用情况", help_text="占用情况", default=False)
+    #fk连接studio
+    #Fk连接用户
+    s_stu = models.ForeignKey('Studio', on_delete=models.CASCADE)
+    s_user = models.ForeignKey('User', on_delete=models.CASCADE, default=None)
+    class Meta:
+        db_table = 'tb_Seat'
+        verbose_name = '座位'
+
+class Studio:
+    Studio_id = models.CharField(max_length=20,help_text="演播厅id",verbose_name="演播厅id")
+    Studio_name= models.CharField(max_length=20, help_text="演播厅名称", verbose_name="演播厅名称")
+    Studio_type = models.CharField(max_length=20, help_text="演播厅类型", verbose_name="演播厅类型")
+    Seating = models.CharField(max_length=20, help_text="座位个数", verbose_name="座位个数")
+
+    class Meta:
+        db_table = 'tb_Studio'
+        verbose_name = '演播厅'
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Users:
+    Users_id = models.CharField(max_length=20,help_text="用户id",verbose_name="用户id")
+    Users_name = models.CharField(max_length=20, verbose_name='用户昵称' , help_text="用户昵称")
+    Name = models.CharField(max_length=20, verbose_name='真实名称', help_text="真实名称")
+    Users_sex = models.BooleanField(verbose_name="性别", help_text="性别", default=True)
+    Users_email = models.CharField(max_length=20, verbose_name='用户邮箱', help_text="用户邮箱")
+    Users_password = models.CharField(max_length=20, verbose_name='用户密码', help_text="用户密码")
+    mobile = models.CharField(max_length=20, verbose_name='电话号码', help_text="电话号码")
+    registration_date = models.CharField(max_length=20, verbose_name='注册日期', help_text="注册日期")
+    ticketmanager = models.BooleanField(verbose_name="票务管理员", help_text="票务管理员", default=False)
+    supermanager = models.BooleanField(verbose_name="超级管理员", help_text="超级管理员", default=False)
+    buymanager = models.BooleanField(verbose_name="售票员", help_text="售票员", default=False)
+
+    class Meta:
+        db_table = 'tb_Users'
+        verbose_name = '用户'
+class Movie:
+    Movie_id = models.CharField(max_length=20,help_text="电影id",verbose_name="电影id")
+    Movie_name = models.CharField(max_length=20, verbose_name='电影名' , help_text="电影名")
+    Movie_time = models.CharField(max_length=20, verbose_name="电影时间",help_text="电影时间")
+
+    class Meta:
+        db_table = 'tb_Movie'
+        verbose_name = '电影'
+
